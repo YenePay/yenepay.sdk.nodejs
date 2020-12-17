e85eb88a859c12e291df76d6143707bd7e4f343c
Yonatan Melka <ybmelka@gmail.com>
2020-12-17 14:53:15 -0600

    changed var to const. added package-lock

--- YenePaySDK Module/yenepaysdk/index.js
+++ YenePaySDK Module/yenepaysdk/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var _checkoutItem = require('./lib/checkoutItem');
-var _checkoutOptions = require('./lib/checkoutOptions');
-var _pdtRequestModel = require('./lib/pdtRequestModel');
-var _checkoutType = require('./lib/checkoutType');
-var querystring = require("querystring");
-var extend =  require("deep-extend");
-var request = require("request");
+const _checkoutItem = require('./lib/checkoutItem');
+const _checkoutOptions = require('./lib/checkoutOptions');
+const _pdtRequestModel = require('./lib/pdtRequestModel');
+const _checkoutType = require('./lib/checkoutType');
+const querystring = require("querystring");
+const extend =  require("deep-extend");
+const request = require("request");
 
 
 function yenepaycheckout(querystring, extend, request)
 {
-  var self = this;
+  const self = this;
   self.querystring = querystring;
   self.extend = extend;
   self.request = request;
@@ -27,11 +27,11 @@ function yenepaycheckout(querystring, extend, request)
   {
     try
       {
-          var item = new _checkoutItem();
+          const item = new _checkoutItem();
           extend(item, checkoutItem);
-          var dict = checkoutOptions.GetAsKeyValue(false);
+          const dict = checkoutOptions.GetAsKeyValue(false);
           item.GetAsKeyValue(dict);
-          var checkoutUrl = self.checkoutBaseUrlProd + "?" + self.querystring.stringify(dict);
+          const checkoutUrl = self.checkoutBaseUrlProd + "?" + self.querystring.stringify(dict);
           if (checkoutOptions.UseSandbox)
               checkoutUrl = self.checkoutBaseUrlSandbox + "?" + querystring.stringify(dict);
           return checkoutUrl;
@@ -46,21 +46,21 @@ function yenepaycheckout(querystring, extend, request)
     {
         try
         {
-            var dict = checkoutOptions.GetAsKeyValue(true);
-            var checkoutUrl = self.checkoutBaseUrlProd + "?" + self.querystring.stringify(dict);
+            const dict = checkoutOptions.GetAsKeyValue(true);
+            const checkoutUrl = self.checkoutBaseUrlProd + "?" + self.querystring.stringify(dict);
             if (checkoutOptions.UseSandbox)
                 checkoutUrl = self.checkoutBaseUrlSandbox + "?" + self.querystring.stringify(dict);
                 
-            for (var i = 0; i < checkoutItems.length; i++)
+            for (const i = 0; i < checkoutItems.length; i++)
             {
-                var item = new _checkoutItem();
+                const item = new _checkoutItem();
                 extend(item, checkoutItems[i]);
-                var itemDict = item.GetAsKeyValue(null);
+                const itemDict = item.GetAsKeyValue(null);
 
                 Object.keys(itemDict).forEach(function(key) {
-                    var val = itemDict[key];
+                    const val = itemDict[key];
                     if(val){
-                        var arr_obj = {};
+                        const arr_obj = {};
                         arr_obj[key] = val;
                         checkoutUrl += '&Items[' + i + '].' + self.querystring.stringify(arr_obj);
                     }
@@ -78,7 +78,7 @@ function yenepaycheckout(querystring, extend, request)
     self.IsIPNAuthentic = function(ipnModel, useSandbox = false)
     {
         return new Promise(function(resolve, reject){
-            var ipnVerifyUrl = useSandbox ? self.ipnVerifyUrlSandbox : self.ipnVerifyUrlProd;
+            const ipnVerifyUrl = useSandbox ? self.ipnVerifyUrlSandbox : self.ipnVerifyUrlProd;
             self.request({
                 url:ipnVerifyUrl, 
                 method:"POST",
@@ -100,7 +100,7 @@ function yenepaycheckout(querystring, extend, request)
     self.RequestPDT = function (pdtRequestModel)
     {
          return new Promise(function(resolve, reject){
-            var pdtUrl = pdtRequestModel.UseSandbox ? self.pdtUrlSandbox : self.pdtUrlProd;
+            const pdtUrl = pdtRequestModel.UseSandbox ? self.pdtUrlSandbox : self.pdtUrlProd;
             self.request({
                 url:pdtUrl, 
                 method:"POST",
@@ -109,7 +109,7 @@ function yenepaycheckout(querystring, extend, request)
             },
                 function (error, response, body) {                    
                     if (!error && response.statusCode == 200) {
-                            var pdtJson = self.PDTStringToJSON(body);
+                            const pdtJson = self.PDTStringToJSON(body);
                             resolve(pdtJson);
                         }
                     else{
@@ -121,8 +121,8 @@ function yenepaycheckout(querystring, extend, request)
     }
 	
 	self.PDTStringToJSON = function(pdtString){
-        var pairs = pdtString.slice(0).split('&');
-        var result = {};
+        const pairs = pdtString.slice(0).split('&');
+        const result = {};
         pairs.forEach(function(pair){
             pair = pair.split('=');
             result[pair[0]] = pair[1];
