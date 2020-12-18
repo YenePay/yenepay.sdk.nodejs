448f8d0ac34e013cca26f8fef17f39ff63566435
Yonatan Melka <ybmelka@gmail.com>
2020-12-17 19:52:12 -0600

    Refactor pdtRequestModel.js to ES6 syntax

--- YenePaySDK Module/yenepaysdk/lib/pdtRequestModel.js
+++ YenePaySDK Module/yenepaysdk/lib/pdtRequestModel.js
@@ -1,22 +1,21 @@
 'use strict';
 
-module.exports = function(pdtToken, transactionId, merchantOrderId, useSandbox = false)
-    {
-       var self = this;
-        self.RequestType = "PDT";
-        self.PdtToken = pdtToken;
-        self.TransactionId = transactionId;
-        self.MerchantOrderId = merchantOrderId;
-        self.UseSandbox = useSandbox;
+module.exports = class {
+  constructor(pdtToken, transactionId, merchantOrderId, useSandbox = false) {
+    this.RequestType = 'PDT';
+    this.PdtToken = pdtToken;
+    this.TransactionId = transactionId;
+    this.MerchantOrderId = merchantOrderId;
+    this.UseSandbox = useSandbox;
+  }
 
-        self.GetPDTDictionary = function()
-        {            
-            var dic = {
-                RequestType: self.RequestType,
-                PdtToken: self.PdtToken,
-                TransactionId: self.TransactionId,
-                MerchantOrderId : self.MerchantOrderId
-            };
-            return dic;
-        }
-    }
+  GetPDTDictionary() {
+    const dic = {
+      RequestType: this.RequestType,
+      PdtToken: this.PdtToken,
+      TransactionId: this.TransactionId,
+      MerchantOrderId: this.MerchantOrderId,
+    };
+    return dic;
+  }
+};
